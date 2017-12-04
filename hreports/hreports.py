dbb899ce3fca40f74ff557ec1724316d95bfef34
Michael Martinides <michael@martinides.de>
2017-12-04 22:50:11 +0100

    Adds Create class for basic query creation

--- hreports/hreports.py
+++ hreports/hreports.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import subprocess
+
+
+class Create(object):
+    def __init__(self, name=None, query=None):
+        self.name = name
+        self.query = query
+
+    def run(self):
+        cmd = 'hledger %s' % self.query
+        output = subprocess.check_output(cmd.split(' '))
+        return output
