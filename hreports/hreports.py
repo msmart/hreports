ade7d28c45a7537fc4b7b2c47d66c9acc1e21fb7
Michael Martinides <michael@martinides.de>
2017-12-14 12:59:31 +0100

    Adds context population with config data, simplifies get_output and render

--- hreports/hreports.py
+++ hreports/hreports.py
@@ -32,6 +32,9 @@ class Hreport(object):
         self.env.filters['datetime'] = datetimeformat
         self.env.filters['german_float'] = german_float
 
+    def get_global_config(self):
+        return self.config.data.get('global', None)
+
     def get_report_config(self, name):
         return self.config.data.get('reports').get(name, None)
 
@@ -49,42 +52,75 @@ class Hreport(object):
     def get_global_config_value(self, key):
         return self.config.data.get('global').get(key, None)
 
-    def get_output(self, query, ledger=False):
+    def run(self, name=False, query=False, ledger=False):
+        if name:
+            query = self.get_report_config_value(name, 'query')
+            ledger = self.get_report_config_value(name, 'ledger')
+
+        if not ledger:
+            ledger = self.get_global_config_value('ledger')
+
+        query = self.env.from_string(query)
+        query = query.render(self.get_context(name))
+
         if ledger:
             cmd = 'hledger -f %s %s' % (ledger, query)
         else:
             cmd = 'hledger %s' % query
+        self.config.cmd = cmd
         return unicode(subprocess.check_output(cmd.split(' ')), 'utf-8')
 
-    def run_query(self, query, ledger=False):
-        if not ledger:
-            ledger = self.get_global_config_value('ledger')
-        query = self.env.from_string(query)
-        query = query.render(self.get_context())
-        return self.get_output(query, ledger)
-
-    def run(self, name):
-        query = self.get_report_config_value(name, 'query')
-        ledger = self.get_report_config_value(name, 'ledger')
-        return self.run_query(query, ledger)
-
-    def get_context(self, name=False):
-        builtins = {'now': datetime.datetime.now()}
-        report_config = self.get_report_config(name)
-        context = {'report': report_config,
-                   'hreport': self,
-                   'global': self.config.data.get('global')}
-
-        context.update(builtins)
-        if report_config:
-            context.update(report_config.get('variables', {}))
+    def render_strings_in_dict(self, data_dict, context, section=False):
+        if not data_dict:
+            return context
+        for key, value in data_dict.items():
+            if isinstance(value, str):
+                value_template = self.env.from_string(value)
+                data_dict[key] = value_template.render(context)
+        if section:
+            context.update({section: data_dict})
+        else:
+            context.update(data_dict)
         return context
 
-    def render(self, name, template_name=None):
+    def get_context(self, name=False):
+        """Build context data for the template.
 
-        if not template_name:
-            template_name = self.get_report_config_value(name,
-                                                         'template')
+        The template context dict is evaluated in this order:
+
+            1. Builtins at {}
+            2. variables of the global section at {}
+            3. global section at {'global'}
+            4. variables of the report section {'report'}
+            5 report section at {}
+        """
+        context = {}
+
+        builtins = {'now': datetime.datetime.now()}
+        context.update(builtins)
+
+        global_config = self.get_global_config()
+        if 'variables' in global_config:
+            global_variables = global_config.get('variables', {})
+            context = self.render_strings_in_dict(global_variables,
+                                                  context)
+        self.render_strings_in_dict(global_config, context, 'global')
+
+        report_config = self.get_report_config(name)
+        if report_config:
+            report_variables = report_config.get('variables', {})
+            self.render_strings_in_dict(report_variables, context)
+
+        self.render_strings_in_dict(report_config, context, 'report')
+
+        context.update({'hreport': self})
+
+        return context
+
+    def render(self, name):
+
+        template_name = self.get_report_config_value(name, 'template')
+
         if not template_name:
             return self.run(name)
 
