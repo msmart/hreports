5c0be6b485e8c017a1919106524b8b8f5b3a11d4
Michael Martinides <michael@martinides.de>
2017-12-21 13:22:29 +0100

    Removes blank line at end of file for flake8

--- hreports/template_filters.py
+++ hreports/template_filters.py
@@ -16,4 +16,3 @@ def german_float(value):
 def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
     return next_month - datetime.timedelta(days=next_month.day)
-
