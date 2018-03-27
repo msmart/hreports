5b15d405cccfbb4f58370634ab5193fd12e0542f
Michael Martinides <michael@martinides.de>
2018-03-27 22:31:50 +0200

    Adds WIP on hreports tests

--- tests/test_hreports.py
+++ tests/test_hreports.py
@@ -5,9 +5,10 @@
 
 
 import unittest
+import tempfile
 from click.testing import CliRunner
 
-from hreports import cli
+from hreports import cli, hreports
 
 
 class TestHreports(unittest.TestCase):
@@ -18,9 +19,6 @@ class TestHreports(unittest.TestCase):
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
 
     def test_command_line_interface(self):
         """Test the CLI."""
@@ -33,6 +31,22 @@ class TestHreports(unittest.TestCase):
         assert help_result.exit_code == 0
         assert 'Show this message and exit.' in help_result.output
 
-        help_result = runner.invoke(cli.main, ['show'])
-        assert help_result.exit_code == 0
-        assert 'reports' in help_result.output
+        show_result = runner.invoke(cli.main, ['show'])
+        self.assertEqual(show_result.exit_code, 0)
+        self.assertIn('reports', show_result.output)
+
+    def test_config(self):
+        runner = CliRunner()
+        config = tempfile.NamedTemporaryFile()
+
+        runner.invoke(cli.main, ['-c', config.name, 'create', 'test'])
+        result = runner.invoke(cli.main, ['-c', config.name,
+                                          '--verbose', '-r'])
+        self.assertIn(config.name, result.output)
+
+    def test_get_global_config(self):
+        from hreports import config
+        test = config.Config()
+        hreport = hreports.Hreport(test)
+        config = hreport.get_global_config()
+        self.assertIsInstance(config, dict)
